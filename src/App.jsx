7103cf64897c2108656941e97b6439cdc28cfef7
Tharusha12-se <146365731+Tharusha12-se@users.noreply.github.com>
2025-09-09 09:50:52 +0530

    hero_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from "./components/header"
+import HeroSection from "./components/HeroSection"
 
 export default function App() {
   return (
+    <>
     <Header />
+    <HeroSection />
+    </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { a } from "framer-motion/client";
-import { FiGithub, FiTwitter, FiLinkedin } from "react-icons/fi";
+import { FiGithub, FiTwitter, FiLinkedin , FiMenu, FiX} from "react-icons/fi";
+import { useState } from "react";
 const Header = () => {
+
+    //Menu open close
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const [constFormOpen, setContactFormOpen] = useState(false);
+
+    const openContactForm = () => setContactFormOpen(true);
+    const closeContactForm = () => setContactFormOpen(false);
+
     return (
         <header className="absolute w-full z-50 transition-all duration-300">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -85,6 +95,7 @@ const Header = () => {
 
                     {/* Hire me  */}
                     <motion.button
+                    onClick={openContactForm}
                         initial={{ opacity: 0, scale: 0.8 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{
@@ -99,10 +110,150 @@ const Header = () => {
             duration-500">
                         Hire Me
                     </motion.button>
-
                 </div>
 
+                {/* Mobile Menu Button */}
+                <div className="md:hidden flex items-center">
+                    <motion.button 
+                    whileTap={{scale: 0.7}}
+                    onClick={toggleMenu}
+                    className="text-gray-300" >
+
+                        {isOpen ? <FiX className="w-6 h-6"/> : <FiMenu className="w-6 h-6"/>}
+                    </motion.button>
+                </div> 
             </div>
+
+            {/* Mobile Menu */}
+            <motion.div 
+            initial={{opacity:0, height:0}}
+            animate={{
+                opacity:isOpen ? 1 : 0,
+                height: isOpen ? "auto" : 0, 
+            }}
+            transition={{duration:0.5}}
+            className="md:hidden overflow-hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5">
+
+                <nav className=" flex flex-col space-y-3">
+                    {["Home", "About", "Project", "Experince", "Contact"].map((item) => (
+                        <a className="text-gray-300 font-medium py-2"
+                        onClick={toggleMenu}
+                        kay = {item} href="#">
+                            {item}
+                        </a>
+                    ))}
+                </nav>
+
+                <div className="pt-4 border-t border-gray-200 dark:border-gray-700">
+                    <div className="flex space-x-5">
+
+                        <a href="#">
+                            <FiTwitter className="w-5 h-5"/>
+                        </a>
+
+                         <a href="#">
+                            <FiLinkedin className="w-5 h-5"/>
+                        </a>
+
+                         <a href="#">
+                            <FiGithub className="w-5 h-5"/>
+                        </a>
+
+                    </div>
+
+                    <button 
+                    onClick={() => {
+                        toggleMenu()
+                        openContactForm()
+                    }}
+                    className="mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r
+                    from-violet-600 to-violet-400 font-bold">
+                        Contact Me
+                    </button>
+                </div>
+            </motion.div>
+
+            {/* Contact form */}
+          <AnimatePresence>
+            {constFormOpen && (
+            <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+            className="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex 
+            items-center justify-center p-4"
+            onClick={closeContactForm}
+            >
+
+                <motion.div 
+                initial={{scale: 0.8, opacity:0, y:30}}
+                animate={{scale:1, opacity:1, y:0}}
+                exit={{scale:0.8, opacity:0, y:30}}
+                transition={{
+                    type:"spring",
+                    damping: 30,
+                    stiffness: 200,
+                    duration: 0.8,
+                }}
+                className="bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6">
+                    <div className="flex justify-between items-center mb-4">
+                        <h1 className="text-2xl font-bold text-gray-300">
+                            Get In Touch
+                        </h1>
+                        <button onClick={closeContactForm}>
+                            <FiX className="w-5 h-5 text-gray-300 font-extrabold" />
+                        </button>
+                    </div>
+
+                    {/* Input Form  */}
+                    <form action="" className="space-y-4">
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium text-gray-300 mb-1">
+                                Name
+                            </label>
+                            <input type="text" id="name" placeholder="Your name"
+                            className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                            focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">
+                                Email
+                            </label>
+                            <input type="email" id="email" placeholder="Your email"
+                            className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                            focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="message" className="block text-sm font-medium text-gray-300 mb-1">
+                                Messege
+                            </label>
+                            <textarea rows="4" type="text" id="message" placeholder="How can we help you?"
+                            className="w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2
+                            focus:ring-violet-500 focus:border-violet-500 bg-gray-700"/>
+                        </div>
+
+                        <motion.button
+                        type="submit"
+                        whileHover={{scale:1.03}}
+                        whileTap={{scale:0.97}}
+                        className="w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400
+                        hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg
+                        shadow-md hover:shadow-lg hover:shadow-violet-600/50">
+                            Send Message
+                        </motion.button>
+
+                    </form>
+
+                </motion.div>
+            
+            </motion.div>
+            )}
+            </AnimatePresence>  
+
+
         </header>
     )
 }
